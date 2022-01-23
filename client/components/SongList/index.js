b7a227e73d0f6fe06a3c6d82931392669c3b6ea8
Puneet Ahuja <puneetahuja@adobe.com>
2022-01-24 00:01:10 +0530

    Added Delete Feature

--- client/components/SongList/index.js
+++ client/components/SongList/index.js
@@ -1,17 +1,32 @@
 import React, {Component} from 'react';
 import { graphql } from 'react-apollo';
 import { Link } from 'react-router';
+import gql from 'graphql-tag';
 
 import query from '../../queries/fetchSongs';
 
 
 class SongList extends Component{
 
+    onSongDelete(id){
+        this.props.mutate({
+            variables: {
+                id,
+            }
+        }).then(()=>{
+            this.props.data.refetch();
+        })
+    }
+
     renderSongs() {
-        return this.props.data.songs.map(song => {
+        return this.props.data.songs.map(({id, title}) => {
             return (
-                <li key={song.id} className="collection-item">
-                    {song.title}
+                <li key={id} className="collection-item">
+                    {title}
+                    <i
+                        className={'material-icons'}
+                        onClick={()=>this.onSongDelete(id)}
+                    >delete</i>
                 </li>
             )
         })
@@ -38,4 +53,12 @@ class SongList extends Component{
     }
 }
 
-export default graphql(query)(SongList);
+const mutation = gql`mutation DeleteSong($id: ID){
+    deleteSong(id: $id){
+        id
+    }
+}`
+
+export default graphql(mutation)(
+    graphql(query)(SongList)
+);
