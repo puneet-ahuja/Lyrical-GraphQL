c2498a1230377e838d34e75826afb56866c6ae85
Puneet Ahuja <puneetahuja@adobe.com>
2022-01-23 21:12:37 +0530

    Added create song feature

---
+++ client/components/SongCreate/SongCreate.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
+import { Link, hashHistory } from 'react-router';
+
+class SongCreate extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            title : ''
+        }
+    }
+
+    onSubmit(event){
+        event.preventDefault();
+
+        this.props.mutate({
+            variables: {
+                title: this.state.title
+            }
+        }).then(
+            () => hashHistory.push('/')
+        ).catch(
+            ()=>{
+                // TODO: We can handle server side or validation from here.
+            }
+        )
+    }
+
+    render(){
+        return (
+            <div >
+                <Link
+                 to="/"
+                >Back</Link>
+                <h3>Create a New Song</h3>
+                <form onSubmit={this.onSubmit.bind(this)}>
+                    <label>Song Title:</label>
+                    <input 
+                        value={this.state.title}
+                        onChange={event => this.setState({
+                            title: event.target.value
+                        })}
+                    />
+                </form>
+            </div>
+        )
+    }
+}
+
+const mutation = gql`
+mutation AddSong($title: String) {
+    addSong(title: $title){
+        id
+        title
+    }
+}
+`
+
+export default graphql(mutation)(SongCreate);
--- client/index.js
+++ client/index.js
@@ -2,8 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ApolloClient from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
+import { Router, Route, hashHistory, IndexRoute } from 'react-router';
 
 import SongList from './components/SongList';
+import App from './components/App';
+import SongCreate from './components/SongCreate/SongCreate';
 
 
 const client = new ApolloClient({});
@@ -11,7 +14,13 @@ const client = new ApolloClient({});
 const Root = () => {
   return (
     <ApolloProvider client={client}>
-      <SongList />
+      <Router history={hashHistory}>
+        <Route path="/" component={App}>
+          {/* TODO: Need to understand this */}
+          <IndexRoute component={SongList} />
+          <Route path="songs/new" component={SongCreate}/>
+        </Route>
+      </Router>
     </ApolloProvider>
   )
 };
